106bc9580d2caf3dec535261541d9bf576e96f78
Christian Lehr <christian_lehr.1@gmx.de>
2019-05-25 11:30:28 +0200

    rename components

---
+++ components/content/Roles.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="justify shorten-80 center margin-bottom-5">
+    <IconMentor
+      :subpoints="[
+        'Actively develops with the other members in a group of max 6 people',
+        'Assists and cares for the other members in every questions regarding IT'
+      ]"
+    />
+    <IconMentee
+      :subpoints="[
+        'Decides with the other members on a project they want to develop together',
+        'Grows and connects with other people',
+        'Makes first experience in IT in a helpful and caring community'
+      ]"
+    />
+    <IconTeamCoach
+      :subpoints="[
+        'Guides the others through the team project',
+        'Is the connection between other teams',
+        'Cares for organizational things regarding the team',
+        'Does retrospectives with the team with the goal to make everyone happier'
+      ]"
+    />
+    <IconOrganizer
+      :subpoints="[
+        'Assists in organizing meetups and talks',
+        'Actively helps to grow DevConnect',
+        'Helps with the social media presence'
+      ]"
+    />
+  </div>
+</template>
+<script>
+import IconMentee from '../icons/IconMentee'
+import IconMentor from '../icons/IconMentor'
+import IconOrganizer from '../icons/IconOrganizer'
+import IconTeamCoach from '../icons/IconTeamCoach'
+
+export default {
+  name: 'Roles',
+  components: { IconMentee, IconMentor, IconOrganizer, IconTeamCoach }
+}
+</script>
+<style>
+figure {
+  display: inline-block;
+}
+
+.justify {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  flex-flow: column;
+}
+
+.shorten-80 {
+  width: 80%;
+}
+
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.margin-bottom-5 {
+  margin-bottom: 5%;
+}
+
+@media only screen and (min-width: 768px) {
+  .justify {
+    flex-flow: row;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -41,36 +41,7 @@
     <MissionContent class="margin-bottom-5" />
 
     <Divider content="Roles" />
-    <div class="justify shorten-80 center margin-bottom-5">
-      <IconMentor
-        :subpoints="[
-          'Actively develops with the other members in a group of max 6 people',
-          'Assists and cares for the other members in every questions regarding IT'
-        ]"
-      />
-      <IconMentee
-        :subpoints="[
-          'Decides with the other members on a project they want to develop together',
-          'Grows and connects with other people',
-          'Makes first experience in IT in a helpful and caring community'
-        ]"
-      />
-      <IconTeamCoach
-        :subpoints="[
-          'Guides the others through the team project',
-          'Is the connection between other teams',
-          'Cares for organizational things regarding the team',
-          'Does retrospectives with the team with the goal to make everyone happier'
-        ]"
-      />
-      <IconOrganizer
-        :subpoints="[
-          'Assists in organizing meetups and talks',
-          'Actively helps to grow DevConnect',
-          'Helps with the social media presence'
-        ]"
-      />
-    </div>
+    <Roles />
 
     <Divider content="Get involved" />
     <GetInvolvedContent class="margin-bottom-5" />
@@ -83,22 +54,16 @@
 </template>
 
 <script>
-import IconOrganizer from '../components/icons/IconOrganizer'
-import IconTeamCoach from '../components/icons/IconTeamCoach'
-import IconMentor from '../components/icons/IconMentor'
-import IconMentee from '../components/icons/IconMentee'
-import AboutUsContent from '../components/content/AboutUsContent'
-import GetInvolvedContent from '../components/content/GetInvolvedContent'
-import MissionContent from '../components/content/MissionContent'
+import AboutUsContent from '../components/content/AboutUs'
+import GetInvolvedContent from '../components/content/GetInvolved'
+import MissionContent from '../components/content/OurMission'
 import Impressum from '../components/content/Impressum'
 import Divider from '../components/Divider'
+import Roles from '../components/content/Roles'
 
 export default {
   components: {
-    IconOrganizer,
-    IconTeamCoach,
-    IconMentor,
-    IconMentee,
+    Roles,
     AboutUsContent,
     GetInvolvedContent,
     MissionContent,
@@ -141,17 +106,6 @@ figure {
   display: inline-block;
 }
 
-.justify {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  flex-flow: column;
-}
-
-.shorten-80 {
-  width: 80%;
-}
-
 .center {
   margin-left: auto;
   margin-right: auto;
@@ -173,11 +127,5 @@ figure {
 .responsive-app {
   max-width: 90%;
   margin: 10% auto 10%;
-}
-
-@media only screen and (min-width: 768px) {
-  .justify {
-    flex-flow: row;
-  }
 }
 </style>
